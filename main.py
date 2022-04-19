2af6e4d6830856224d52786a862c4b9cf2336f40
ANEESH PABOLU PABOLU <104022036+APCODEZ@users.noreply.github.com>
2022-04-19 16:34:19 +0000

    Added code and teached some lines of the code

---
+++ main.py
@@ -0,0 +1,9 @@
+#Print hello world in python!
+print("Hello world")
+
+#Getting input statements:
+
+#Getting our first number from the user (no1):
+no1 = int(input('In put your first number here: ', ))
+#Getting our second number from the user (no2):
+no2 = int(input('In put your second number here: '))
